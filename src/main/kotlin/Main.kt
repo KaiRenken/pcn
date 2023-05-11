02a5b021a284461f30298852cb00569771829e07
KaiRenken <kai.renken@gmx.de>
2023-05-11 08:40:46 +0200

    second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    if (args.size != 1) {
+        println("Wrong number of arguments! Exactly one argument required.")
+        return
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    try {
+        val number = args[0].toInt()
+
+        println(computeConsecutivePartitions(number = number))
+    } catch (exception: NumberFormatException) {
+        println("Argument must be a positive integer!")
+    }
+}
+
+fun computeConsecutivePartitions(number: Int): List<List<Int>> {
+    val partitions: MutableList<List<Int>> = ArrayList()
+
+    for (i in 1..number step 2) {
+        if (number % i == 0) partitions.add(computePartition(number = number, divisor = i))
+    }
+
+    return partitions
+}
+
+fun computePartition(number: Int, divisor: Int): List<Int> {
+    return listOf(divisor)
 }
